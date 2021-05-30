d94f5a43fa0ef7d236b7792e5b720f1356f52b46
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-05-30 12:34:48 -0300

    Added states reading

--- Reader.java
+++ Reader.java
@@ -5,17 +5,28 @@ public class Reader {
         AFD afd = new AFD();
         BufferedReader reader = new BufferedReader(new FileReader("gameShooterAFD.txt"));
         
-        String line = "";
-        line = reader.readLine();
+        String line = reader.readLine();
         line = line.replace("{", "").replace("}", "").replace("(", "").replace(")", "");
+     
         String[] titleAndContent = line.split("="); // [0] -> title || [1] -> content
         afd.name = titleAndContent[0];
 
         String[] content = titleAndContent[1].split(",");
-        for (String state : content) {
-            System.out.println(state);
+
+        int endStateIndex = 0;
+        
+        // Add states to the AFD
+        // States name must start with Q
+        for (int i = 0; i < content.length; i++) {
+            if(!content[i].toUpperCase().startsWith("Q")) {
+                endStateIndex = i;
+                break;
+            }
+
+            afd.states.add(content[i]);
         }
 
-
+        System.out.println(afd.states.toString());
+        reader.close();
     }
 }
